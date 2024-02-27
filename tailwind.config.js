8ab0188cfd2afd9a3007737cf013b638a39e7bd6
mamorshedalam <ma.morshedalampro@gmail.com>
2024-02-27 09:16:55 +0800

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  // darkMode: "class",
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Raleway", "ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+    },
+  },
+  plugins: [],
+};
